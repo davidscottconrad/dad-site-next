859d939db1a3abd950a52a4e4db495f85f3f6c55
davidscottconrad <david.scott.conrad@gmail.com>
2024-02-25 18:33:02 -0500

    fixed build issue

--- components/splash.js
+++ components/splash.js
@@ -1,29 +1,43 @@
-import Image from 'next/image'
-import cecLogo from '../public/logocolor2.jpg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
-const element = <FontAwesomeIcon icon={faArrowDown} size='sm' />
+const element = <FontAwesomeIcon icon={faArrowDown} size="sm" />;
 
 function Splash() {
-  return <div>
-
-    <div className='container-fluid' id="splash" >
-      <div className='row'>
-        <div className='col-md-12'>
-          <div id="cecImage">
-            <Image src={cecLogo} layout="responsive" alt="Conrad Eye Centers Logo" />
+  return (
+    <div>
+      <div className="container-fluid" id="splash">
+        <div className="row">
+          <div className="col-md-12">
+            <div id="cecImage">
+              <Image
+                src="/logocolor2.jpg"
+                layout="responsive"
+                height={889}
+                width={2106}
+                alt="Conrad Eye Centers Logo"
+              />
+            </div>
+            <h1
+              className="text-center"
+              style={{ paddingTop: "5vh", fontWeight: "bold" }}
+            >
+              Ophthalmology
+            </h1>
+            <h1 className="text-center" style={{ fontWeight: "bold" }}>
+              Eye Diseases and Surgery
+            </h1>
+            <h1 className="text-center" style={{ fontWeight: "bold" }}>
+              Tim Conrad, MD
+            </h1>
           </div>
-          <h1 className='text-center' style={{ paddingTop: "5vh", fontWeight: "bold" }} >Ophthalmology</h1>
-          <h1 className='text-center' style={{ fontWeight: "bold" }} >Eye Diseases and Surgery</h1>
-          <h1 className='text-center' style={{ fontWeight: "bold" }}>Tim Conrad, MD</h1>
         </div>
       </div>
+
+      <div id="buffer"></div>
     </div>
-    
-    <div id="buffer">
-    </div>
-  </div>
+  );
 }
 
-export default Splash
+export default Splash;
