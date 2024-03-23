07f2f98988738a1c0f6ace02f958d06491968b67
davidscottconrad <david.scott.conrad@gmail.com>
2024-03-23 14:42:21 -0400

    springs

--- components/contact.js
+++ components/contact.js
@@ -2,31 +2,38 @@ function Contact() {
     return (
         <div className="container-fluid p-4" id="contact">
             <div className="row">
-                <div className="col-md-6 mb-3">
-                    <h1 style={{ fontSize: '36px' }}>Locations</h1>
+                <div className="col-md-6 mb-6">
+                    <h1 style={{ borderBottom: '1px solid black' }}>Locations</h1>
                     <div className="row">
-                        <div className="col-md-6">
-                            <h2 style={{ fontSize: '22px' }}>St. Matthews</h2>
+                        <div className="col-md-6 mb-4">
+                            <h2 >St. Matthews</h2>
                             <p>
                                 204 Breckenridge Lane <br></br> Louisville, KY 40207<br></br>
                             </p>
                         </div>
-                        <div className="col-md-6">
-                            <h2 style={{ fontSize: '22px' }}>New Albany</h2>
+                        <div className="col-md-6 mb-4">
+                            <h2 >New Albany</h2>
                             <p>
                                 Professional Arts Building<br></br>
                                 1919 State Street Suite 201<br></br>
                                 New Albany, IN 47150<br></br>
                             </p>
                         </div>
+                        <div className="col-md-6 mb-4">
+                            <h2>Laurie Wilbanks Springs Location</h2>
+                            <p>
+                                1919 State Street Suite 201<br></br>
+                                Louisville, KY 40205<br></br>
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="col-md-6">
-                    <h1 style={{ fontSize: '36px' }}>Contact Information</h1>
+                    <h1 style={{ borderBottom: '1px solid black' }}>Contact Information</h1>
                     <div className="row">
-                        <div className="col-md-6"><h2 style={{ fontSize: '22px' }}>St. Matthews</h2>Phone: 502-899-7778<br></br>
+                        <div className="col-md-6 mb-4"><h2 >St. Matthews</h2>Phone: 502-899-7778<br></br>
                             Fax: 502-897-5757</div>
-                        <div className="col-md-6"><h2 style={{ fontSize: '22px' }}>New Albany</h2>Phone: 812-944-6063<br></br>
+                        <div className="col-md-6 mb-4"><h2 >New Albany</h2>Phone: 812-944-6063<br></br>
                             Fax: 502-897-5757</div>
                     </div>
                 </div>
