11f3def9257ef950d30cd08497a1e36df037320f
davidscottconrad <david.scott.conrad@gmail.com>
2024-05-18 17:57:31 -0400

    fix for overlapping text?

--- components/services.js
+++ components/services.js
@@ -1,62 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import Collapsible from './Collapsible/collapsible';
+import React, { useState, useEffect } from "react";
+import Collapsible from "./Collapsible/collapsible";
 
 function Services() {
+  return (
+    <div id="services" style={{ position: "relative" }}>
+      <div className="container">
+        <h2 className="text-center pb-5">Services </h2>
+        <div className="row">
+          <Collapsible
+            label="Glasses and Contacts"
+            readMoreLink={"/Services/GlassesAndContacts"}
+          >
+            Conrad Eye Centers focuses on your vision with one-on-one
+            personalized eye exams for any age. Each of our locations has a full
+            service optical boutique.
+          </Collapsible>
 
+          <Collapsible
+            label="Eye Injuries and Infections"
+            readMoreLink={"/Services/EyeInjuries"}
+          >
+            Is your eye red, irritated, scratchy, watery, or painful? Dr. Conrad
+            is your convenient, quick, and knowledgeable answer.
+          </Collapsible>
 
+          <Collapsible
+            label="Laser Vision Correction"
+            readMoreLink={"/Services/Laser"}
+          >
+            <p>Lasik and PRK</p>
 
-  return <div id="services">
+            <p>Reduce your need for glasses or contacts</p>
+          </Collapsible>
+        </div>
 
-    <div className="container">
-      <h2 className="text-center pb-5">Services </h2>
-      <div className='row'>
+        <div className="row">
+          <Collapsible
+            label="Eye Diseases"
+            readMoreLink={"/Services/EyeDiseases"}
+          >
+            Conrad Eye Centers can provide you the best care and treatment in
+            Louisville for chronic eye diseases such as glaucoma, macular
+            degeneration, diabetic retinopathy, and many others.
+          </Collapsible>
 
-        <Collapsible label="Glasses and Contacts" readMoreLink={'/Services/GlassesAndContacts'}>
-        Conrad Eye Centers focuses on your vision with one-on-one personalized eye exams for any age. Each of our locations has a full service optical boutique. 
-        </Collapsible>
+          <Collapsible label="Eye Implants" readMoreLink={"/Services/Implants"}>
+            Conrad Eye Centers uses the most advanced multi-focal lens implant
+            technology in intraocular lens replacement for cataracts.
+          </Collapsible>
 
-        <Collapsible label="Eye Injuries and Infections" readMoreLink={'/Services/EyeInjuries'}>
-          Is your eye red, irritated, scratchy, watery, or painful? Dr. Conrad is your convenient, quick, and knowledgeable answer. 
-        </Collapsible>
+          <Collapsible
+            label="Surgical Procedures"
+            readMoreLink={"/Services/SurgicalProcedures"}
+          >
+            Cataract Surgery, Chalazion/Stye Excision, and Skin Tag Removal
+          </Collapsible>
+        </div>
 
-        <Collapsible label="Laser Vision Correction" readMoreLink={'/Services/Laser'}>
-        <p>Lasik and PRK</p>
-
-        <p>Reduce your need for glasses or contacts</p> 
-        </Collapsible>
-      </div>
-
-      <div className='row'>
-        <Collapsible label="Eye Diseases" readMoreLink={'/Services/EyeDiseases'}>
-          Conrad Eye Centers can provide you the best care and treatment in Louisville for chronic eye diseases such as glaucoma, macular degeneration, diabetic retinopathy, and many others.
-        </Collapsible>
-
-        <Collapsible label="Eye Implants" readMoreLink={'/Services/Implants'}>
-        Conrad Eye Centers uses the most advanced multi-focal lens implant technology in intraocular lens replacement for cataracts.
-        </Collapsible>
-
-        <Collapsible label="Surgical Procedures" readMoreLink={'/Services/SurgicalProcedures'}>
-        Cataract Surgery, Chalazion/Stye Excision, and Skin Tag Removal
-        </Collapsible>
-      </div>
-
-      <div className='row'>
-        
-
-        {/* <Collapsible label="Pediatric Opthalmology" readMoreLink={'/Services/Pediactric'}>
+        <div className="row">
+          {/* <Collapsible label="Pediatric Opthalmology" readMoreLink={'/Services/Pediactric'}>
           Dr. Conrad sees patients of all ages, and has had additional training in pediatric eye diseases.
         </Collapsible> */}
 
-        {/* <Collapsible label="Cosmetics" readMoreLink={'/Services/Cosmetics'}>
+          {/* <Collapsible label="Cosmetics" readMoreLink={'/Services/Cosmetics'}>
           Latisse and Botox
         </Collapsible> */}
+        </div>
       </div>
-
-
-
-
     </div>
-  </div>
+  );
 }
 
-export default Services
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Services from '../components/services'
-import About from '../components/about'
-import Navigation from '../components/navigation'
-import Splash from '../components/splash'
-import Resources from '../components/resources'
-import Contact from '../components/contact'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from "next/head";
+import Services from "../components/services";
+import About from "../components/about";
+import Navigation from "../components/navigation";
+import Splash from "../components/splash";
+import Resources from "../components/resources";
+import Contact from "../components/contact";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
-
   return (
-
     <div>
       <Head>
         <title>Conrad Eye Centers</title>
@@ -20,42 +18,47 @@ export default function Home() {
         <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href='https://fonts.googleapis.com/css2?family=Balthazar&display=swap' rel="stylesheet" />
-        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Balthazar&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="path/to/font-awesome/css/font-awesome.min.css"
+        ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <div className="header-container">
         <Navigation></Navigation>
         <Splash></Splash>
-        <div>
-        <h1
-                className="text-center"
-                style={{ paddingTop: "5vh", fontWeight: "bold" }}
-              >
-                Ophthalmology
-              </h1>
-              <h1 className="text-center" style={{ fontWeight: "bold" }}>
-                Eye Diseases and Surgery
-              </h1>
-              <h1 className="text-center" style={{ fontWeight: "bold" }}>
-                Glaucoma
-              </h1>
-              <br />
-              <h1 className="text-center" style={{ fontWeight: "bold" }}>
-                Tim Conrad, MD
-              </h1>
-              <h1 className="text-center" style={{ fontWeight: "bold" }}>
-                Laurie Wilbanks, MD
-              </h1>
+        <div style={{ position: "relative" }}>
+          <h1
+            className="text-center"
+            style={{ paddingTop: "5vh", fontWeight: "bold" }}
+          >
+            Ophthalmology
+          </h1>
+          <h1 className="text-center" style={{ fontWeight: "bold" }}>
+            Eye Diseases and Surgery
+          </h1>
+          <h1 className="text-center" style={{ fontWeight: "bold" }}>
+            Glaucoma
+          </h1>
+          <br />
+          <h1 className="text-center" style={{ fontWeight: "bold" }}>
+            Tim Conrad, MD
+          </h1>
+          <h1 className="text-center" style={{ fontWeight: "bold" }}>
+            Laurie Wilbanks, MD
+          </h1>
         </div>
       </div>
       <Services></Services>
-      <About ></About>
+      <About></About>
       <Resources></Resources>
       <Contact></Contact>
-
     </div>
-  )
+  );
 }
- // style={{position:"absolute", top:"35vh"}}
+// style={{position:"absolute", top:"35vh"}}
